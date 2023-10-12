2c0babbd6d339307a909027c14f60bf02d79729f
DemonXlegenD <flahalle@gaming.tech>
2023-10-12 19:32:25 +0200

    FIX: Réparation des fichiers
    -Réparations des fichiers après merge

--- PROJECT 1/Player/PlayerWizard.h
+++ PROJECT 1/Player/PlayerWizard.h
@@ -1,63 +1,34 @@
-#include "PlayerWizard.h"
-#include <vector>
-
-PlayerWizard::PlayerWizard(
-	const std::string id,
-	const std::string name,
-	int pvMax,
-	int attackMax,
-	int attack,
-	int defenseMax,
-	int defense,
-	int manaMax
-) : PlayerAbstract(
-	id,
-	name,
-	pvMax,
-	attackMax,
-	attack,
-	defenseMax,
-	defense,
-	manaMax,
-	std::string("Wizard")
-)
-{}
+#pragma once
+#include "../PlayerAbstract.h"
 
 
-const std::vector<std::string> actions = {
-	"Avadacadavra",
-	"Mana shield",
-	"Attack Debuff",
-	"Attack Buff",
-	"Surrend",
+class PlayerWizard : public PlayerAbstract
+{
+	using PlayerAbstract::PlayerAbstract;
+
+public:
+	
+	PlayerWizard(
+		const std::string name,
+		int pv_max,
+		int attack_max,
+		int attack,
+		int defense_max,
+		int defense,
+		int mana_max
+	);
+
+	int useAvadacadavra();
+	void surrender();
+	void useManaShield();
+	void useAtkDebuff();
+	void useAtkBuff();
+
+	void useActions(int index);
+
+	
+
+private:
+	
 };
 
-int PlayerWizard::useAvadacadavra() {
-
-	return attack * 2;
-}
-
-void PlayerWizard::useManaShield() {
-	std::cout << "Mana shield Used\n";
-}
-
-void PlayerWizard::useAtkBuff() {
-	std::cout << "Attack Buffed\n";
-}
-
-void PlayerWizard::useAtkDebuff() {
-	std::cout << "Ennemy Attack Debuffed\n";
-}
-
-void PlayerWizard::surrender() {
-	std::cout << "You have surrendered the fight\n";
-}
-
-void PlayerWizard::useActions(int index) {
-	if (index >= 0 && index < actions.size()) {
-		std::cout << actions[index] << " est ex?cut?e" << std::endl;
-	}
-	else {
-		std::cout << "Indice d'action invalide." << std::endl;
-	}
-}
